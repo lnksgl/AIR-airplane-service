57dcded14a062b0928d26f64639322f425b9f487
lnksgl <alexshlyck@gmail.com>
2022-07-27 16:05:18 +0300

    adding crud and migration

--- src/main/java/avia/flight_service/AirplaneServiceApplication.java
+++ src/main/java/avia/flight_service/AirplaneServiceApplication.java
@@ -1,4 +1,4 @@
-package avia.airplane_service;
+package avia.flight_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
